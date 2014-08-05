89f07468e9de98af9c0e7a3cd0a4fcd6d4df8074
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-08-05 14:09:27 +0530

    showing oncall customer list in filtered table

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -45,8 +45,26 @@ angular.module("app.controllers", []).controller("AppCtrl", ["$scope", "$locatio
             
         }
 
-        //angular.element("#customer_id").val()
-
+        $http.get('api/getOncallCustomerList.php').success(function(data){
+          console.log(data);
+            $scope.list = data;
+            $scope.currentPage = 1; //current page
+            $scope.entryLimit = 5; //max no of items to display in a page
+            $scope.filteredItems = $scope.list.length; //Initially for no filter  
+            $scope.totalItems = $scope.list.length;
+        });
+        $scope.setPage = function(pageNo) {
+            $scope.currentPage = pageNo;
+        };
+        $scope.filter = function() {
+            $timeout(function() { 
+                $scope.filteredItems = $scope.filtered.length;
+            }, 10);
+        };
+        $scope.sort_by = function(predicate) {
+            $scope.predicate = predicate;
+            $scope.reverse = !$scope.reverse;
+        };
         $scope.processNewCustomerForm = function(selectedCustomer) {
             $rootScope.existing_customer_id = selectedCustomer['customer_id'];
             $scope.newCustomer.customer_name = selectedCustomer['customer_name'];
@@ -451,14 +469,21 @@ angular.module("app.controllers", []).controller("AppCtrl", ["$scope", "$locatio
 
 }]).controller("adminSettingsCtrl", ["$scope", "$http", function($scope, $http) {
 
-}]).service("timeDifference", function() {
+}]).filter('startFrom', function() {
+    return function(input, start) {
+        if(input) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+        return [];
+    }
+}).service("timeDifference", function() {
     return {
         diffTime: function () {
         }     
  
     };
 });
-
 
 
 
